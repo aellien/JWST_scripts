a2e75deedcc01faee86b6287dd7faf854d49b260
ellien <ellien@iap.fr>
2022-12-06 18:56:58 +0100

    fix typo name

--- launch_dawis_jwst.sh
+++ launch_dawis_jwst.sh
@@ -5,6 +5,6 @@
 for file in f277_rot.fits f356_rot.fits f444_rot.fits
 do
       echo "Launch Dawis on file ${file}"
-      qsub qsub_dawis.sh -v ncl=${n:0:-5}
+      qsub qsub_dawis_jwst.sh -v ncl=${n:0:-5}
       sleep 2
 done
