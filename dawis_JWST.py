e8ba714974c2a361ed6cf5a6dd8760df1585c96f
ellien <ellien@iap.fr>
2023-04-06 15:53:30 +0200

    out7

--- dawis_JWST.py
+++ dawis_JWST.py
@@ -6,16 +6,16 @@ import shutil
 indir = '/n03data/ellien/JWST/data'
 infile = sys.argv[1]
 outdir = '/n03data/ellien/JWST/wavelets/out6/'
-n_cpus = 1 # Number of CPUs
+n_cpus = 2 # Number of CPUs
 tau = 0.9   # Relative Threshold
 gamma = 0.2   # Attenuation (CLEAN) factor
-ceps = 1E-3    # Convergence value for epsilon
+ceps = 1E-4    # Convergence value for epsilon
 n_levels = 9    # Number of wavelet scales
 min_span = 1    # Minimum of wavelet scales spanned by an interscale tree (must be >= 1)
 max_span = 3    # Maximum number of wavelet scales spanned by an interscale tree
 lvl_sep_big = 7     # Scale at wich mix_span & max_span are set to 1, and gamma to 1
-extent_sep = 1E-10    # Ratio n_pix/vignet under which the Haar wavelet is used for restoration
-lvl_sep_lin = 4     # Wavelet scale under which the Haar wavelet can be used for restoration
+extent_sep = 0.15   # Ratio n_pix/vignet under which the Haar wavelet is used for restoration
+lvl_sep_lin = 3     # Wavelet scale under which the Haar wavelet can be used for restoration
 max_iter = 1500      # Maximum number of iterations
 data_dump = True    # Write data at each iteration /!\ demands lot of space on hardware /!\
 gif = True      # Make gifs of the run (need data_dump = True)
