365df16c233e09750e30d61c9d16d21970f52b2c
ellien <ellien@iap.fr>
2023-04-07 15:26:26 +0200

    out9

--- dawis_JWST.py
+++ dawis_JWST.py
@@ -5,16 +5,17 @@ import shutil
 
 indir = '/n03data/ellien/JWST/data'
 infile = sys.argv[1]
-outdir = '/n03data/ellien/JWST/wavelets/out8/'
+outdir = '/n03data/ellien/JWST/wavelets/out9/'
 n_cpus = 2 # Number of CPUs
 tau = 0.9   # Relative Threshold
 gamma = 0.2   # Attenuation (CLEAN) factor
 ceps = 1E-4    # Convergence value for epsilon
-n_levels = 9    # Number of wavelet scales
+n_levels = 10    # Number of wavelet scales
 min_span = 1    # Minimum of wavelet scales spanned by an interscale tree (must be >= 1)
-max_span = 1    # Maximum number of wavelet scales spanned by an interscale tree
-lvl_sep_big = 7     # Scale at wich mix_span & max_span are set to 1, and gamma to 1
+max_span = 2    # Maximum number of wavelet scales spanned by an interscale tree
+lvl_sep_big = 6     # Scale at wich mix_span & max_span are set to 1, and gamma to 1
 extent_sep = 0.15   # Ratio n_pix/vignet under which the Haar wavelet is used for restoration
+ecc_sep = 0.9      # Eccentricity threshold over which the Haar wavelet is used for restoration
 lvl_sep_lin = 3     # Wavelet scale under which the Haar wavelet can be used for restoration
 max_iter = 1500      # Maximum number of iterations
 data_dump = True    # Write data at each iteration /!\ demands lot of space on hardware /!\
@@ -23,12 +24,12 @@ starting_level = 2 # Starting wavelet scale (this is the third scale - Python co
 conditions = 'prolongation'
 monomodality = True
 resume = True
-rm_gamma_for_big = False
+rm_gamma_for_big = True
 
 shutil.copyfile( os.path.abspath(__file__), os.path.join( outdir, infile[:-4] + 'input.dawis.py' ) )
 
 dawis.synthesis_by_analysis( indir = indir, infile = infile, outdir = outdir, n_cpus = n_cpus, n_levels = n_levels, \
                                     tau = tau, gamma = gamma, ceps = ceps, conditions = conditions, min_span = min_span, \
                                     max_span = max_span, lvl_sep_big = lvl_sep_big, monomodality = monomodality, \
-                                    max_iter = max_iter, extent_sep = extent_sep, rm_gamma_for_big = rm_gamma_for_big, \
+                                    max_iter = max_iter, extent_sep = extent_sep, ecc_sep = ecc_sep, rm_gamma_for_big = rm_gamma_for_big, \
                                     data_dump = data_dump, gif = gif, resume = resume )
