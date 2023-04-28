b60069fcdbd208c8485f32cec22a6216f6562c06
ellien <ellien@iap.fr>
2023-04-28 15:07:26 +0200

    280420231507

--- launch_dawis_jwst.sh
+++ launch_dawis_jwst.sh
@@ -5,7 +5,7 @@
 for f in f444w f277w f356w
 do
       #file=jw02736001001_${f}_bkg_rot_crop_det_nosky.fits
-      file=jw02736001001_${f}_bkg_rot_crop.fits
+      file=jw02736001001_${f}_bkg_rot_crop_input.fits
       echo "Launch Dawis on file $file"
       qsub qsub_dawis_jwst.sh -v n=${file},ncl=${file:0:-5},chan=long
       sleep 2
@@ -14,7 +14,7 @@ done
 for f in f090w f150w f200w
 do
       #file=jw02736001001_${f}_bkg_rot_crop_det_nosky.fits
-      file=jw02736001001_${f}_bkg_rot_crop.fits
+      file=jw02736001001_${f}_bkg_rot_crop_input.fits
       echo "Launch Dawis on file $file"
       qsub qsub_dawis_jwst.sh -v n=${file},ncl=${file:0:-5},chan=short
       sleep 2
