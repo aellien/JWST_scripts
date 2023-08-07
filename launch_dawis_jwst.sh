a8d3576234ac9b90eeb564217d9b21ca2d4ac5cc
ellien <ellien@iap.fr>
2023-08-07 16:58:38 +0200

    out14

--- launch_dawis_jwst.sh
+++ launch_dawis_jwst.sh
@@ -11,11 +11,12 @@
 #      sleep 2
 #done
 
-for f in f090w
+for f in f090w f150w f200w
 do
       #file=jw02736001001_${f}_bkg_rot_crop_det_nosky.fits # out12
       #file=jw02736001001_${f}_bkg_rot_crop_warp_det_nosky_input.fits # out13 - rebinned
-      file=jw02736001001_${f}_bkg_rot_crop_test_clean_det_nosky_input.fits # out14 - test rm scattered light
+      file=jw02736001001_${f}_bkg_rot_crop_warp_nobkg1_det_nosky_input.fits # out14 rm scattered light
+      echo "Launch Dawis on file $file"
       echo "Launch Dawis on file $file"
       qsub qsub_dawis_jwst.sh -v n=${file},ncl=${file:0:-5},chan=long # Long here is just for dawis input file (rebinned short images)
       sleep 2
