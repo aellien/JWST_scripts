5d4a80b68e0425a9253f3085cd9d1336101b7133
ellien <ellien@iap.fr>
2023-08-16 15:16:43 +0200

    true out15

--- launch_dawis_jwst.sh
+++ launch_dawis_jwst.sh
@@ -11,9 +11,9 @@
 #      sleep 2
 #done
 
-for file in jw02736001001_f090w_bkg_rot_crop_warp_nobkg1_det_nosky_input.fits \
-            jw02736001001_f150w_bkg_rot_crop_warp_nobkg1_det_nosky_input.fits \
-            jw02736001001_f200w_bkg_rot_crop_warp_nobkg1_det_nosky_input.fits \
+for file in jw02736001001_f090w_bkg_rot_crop_warp_nobkg2.fits \
+            jw02736001001_f150w_bkg_rot_crop_warp_nobkg2.fits \
+            jw02736001001_f200w_bkg_rot_crop_warp_nobkg2.fits \
             jw02736001001_f277w_bkg_rot_crop_input.fits \
             jw02736001001_f356w_bkg_rot_crop_input.fits \
             jw02736001001_f444w_bkg_rot_crop_input.fits
@@ -21,7 +21,7 @@ do
       #file=jw02736001001_${f}_bkg_rot_crop_det_nosky.fits # out12
       #file=jw02736001001_${f}_bkg_rot_crop_warp_det_nosky_input.fits # out13 - rebinned
       #file=jw02736001001_${f}_bkg_rot_crop_warp_nobkg1_det_nosky_input.fits # out14 rm scattered light
-      # out15 - slightly deeper run, DAWIS ran on every filter again
+      #file=jw02736001001_${f}_bkg_rot_crop_warp_nobkg2.fits # out15 - slightly deeper run, new scattered light bkg, DAWIS ran on every filter again
       echo "Launch Dawis on file $file"
       qsub qsub_dawis_jwst.sh -v n=${file},ncl=${file:0:-5},chan=long # Long here is just for dawis input file (rebinned short images)
 done
