f82f1a66b5812db31dcd7e7aa598d1fee363b7b3
aellien <ellien@iap.fr>
2024-11-20 11:25:24 +0100

    synth

--- synthesis/launch_synthesis_jobs.sh
+++ synthesis/launch_synthesis_jobs.sh
@@ -9,31 +9,31 @@ i=0
 patch=()
 while IFS=' ' read ID chan pixscale pixar n_levels lvl_sep_max mu_lim
 do
+    ((i=i+1))
     # ignore header
     if [[  $ID == "#" ]];then
         continue
     fi
-        # if patch smaller than Npatch..
-        if [ ${#patch[@]} -lt $Npatch ];then
-            echo "Add $ID to patch"
-            patch+=($ID) # ..add cluster to patch
-        else
 
-            # ..else launch patch..
+    echo "$i | Add $ID to patch"
+        patch+=($ID) # ..add cluster to patch
+
+        # if patch greater than Npatch..
+        if [ ${#patch[@]} -ge $Npatch ];then
+
+            #�| ..launch patch..
             echo "Launch ${patch[@]}"
-            sbatch start_synthesis_slurm_job.sh ${patch[@]} 
+            sbatch start_synthesis_slurm_job_new.sh ${patch[@]}
 
             # ..and reset the patch
             patch=()
         fi
-        
 done < $input_list
 
 #Launch last patch
-if [ ${#patch[@]} -lt $Npatch ];then
+if [ ${#patch[@]} -le $Npatch ];then
     echo "Launch ${patch[@]}"
-    sbatch start_synthesis_slurm_job.sh ${patch[@]} 
+    sbatch start_synthesis_slurm_job_new.sh ${patch[@]}
 fi
 
-exit 0
- 
+exit 
